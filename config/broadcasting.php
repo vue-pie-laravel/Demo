caf51f2ebcf47d129b3f7f65a81ba9891524aa9a
Marc Newton <hello@marcnewton.co.uk>
2020-06-18 17:07:39 +0100

    Configure pusher self host.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'useTLS' => false,
+                'host' => env('LARAVEL_WEBSOCKETS_HOST', '127.0.0.1'),
+                'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
+                'scheme' => 'http'
             ],
         ],
 
